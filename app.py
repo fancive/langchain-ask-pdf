bda2d6bd8fe0d4be0065fc8f24c527d2b78e09be
fancivez <fancywu27@163.com>
2023-08-23 10:36:47 +0800

    * feat(app.py): add support for uploading PDFs and asking custom questions * feat(app.py): add support for pre-configured questions * feat(app.py): generate audio responses for questions and answers * feat(app.py): display history of uploaded PDFs and generated audios * fix(app.py): save uploaded PDF to 'uploads' directory * fix(app.py): adjust playback speed of generated audio * fix(app.py): extract text content from uploaded PDF * fix(app.py): save generated audio to 'audios' directory * fix(app.py): adjust paths for ffmpeg and ffprobe executables * feat(app.py): add check for existence of 'uploads' and 'audios' directories * feat(app.py): add function to generate audio for question and answer * feat(app.py): add function to extract text from PDF * feat(app.py): add function to display history of uploaded PDFs and generated audios * feat(app.py): add function to save uploaded PDF * feat(app.py): add

--- app.py
+++ app.py
@@ -1,55 +1,171 @@
+import os
+import io
 from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
+from gtts import gTTS
+from pydub import AudioSegment
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+from pydub import AudioSegment
+import tempfile
+
+AudioSegment.converter = "/opt/homebrew/bin/ffmpeg"
+AudioSegment.ffmpeg = "/opt/homebrew/bin/ffmpeg"
+AudioSegment.ffprobe = "/opt/homebrew/bin/ffprobe"
+
+# Global history list to keep track of uploaded files and generated audios
+history = []
+
+# Ensure the required directories exist
+for directory in ["uploads", "audios"]:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+def generate_audio_for_question_and_answer(q, response):
+    combined_audio = AudioSegment.empty()
+
+    # Convert question to audio and save to temp file
+    question_audio = gTTS(text=q, lang="zh")
+    with tempfile.NamedTemporaryFile(delete=True) as fp:
+        question_audio.save(fp.name)
+        combined_audio += AudioSegment.from_mp3(fp.name)
+
+    # Convert answer to audio and save to temp file
+    answer_audio = gTTS(text=response, lang="zh")
+    with tempfile.NamedTemporaryFile(delete=True) as fp:
+        answer_audio.save(fp.name)
+        combined_audio += AudioSegment.from_mp3(fp.name)
+
+    return combined_audio
+
+
+def extract_text_from_pdf(pdf):
+    """Extract text content from given PDF."""
+    pdf_reader = PdfReader(pdf)
+    return "".join(page.extract_text() for page in pdf_reader.pages)
+
+
+def display_history():
+    # 获取 uploads 和 audios 目录中的所有文件
+    uploaded_pdfs = os.listdir("uploads")
+    generated_audios = os.listdir("audios")
+
+    # 展示每一个文件
+    for pdf_name, audio_name in zip(uploaded_pdfs, generated_audios):
+        pdf_path = os.path.join("uploads", pdf_name)
+        audio_path = os.path.join("audios", audio_name)
+
+        st.markdown(f"### {pdf_name}")
+        with open(audio_path, "rb") as audio_file:
+            audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format="audio/mp3")
+        st.markdown(f"[Download PDF]({pdf_path})")
+
+
+def save_uploaded_pdf(pdf):
+    """Save the uploaded PDF and return its path."""
+    pdf_path = os.path.join("uploads", pdf.name)
+    with open(pdf_path, "wb") as f:
+        f.write(pdf.getvalue())
+    return pdf_path
+
+
+def process_questions_and_generate_audio(pdf_path, questions):
+    """Process the uploaded PDF, ask questions, and generate combined audio for answers."""
+    text = extract_text_from_pdf(pdf_path)
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    llm = OpenAI()
+    chain = load_qa_chain(llm, chain_type="stuff")
+
+    overall_audio = AudioSegment.empty()
+    for q in questions:
+        docs = knowledge_base.similarity_search(q)
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=q)
+
+        question_and_answer_audio = generate_audio_for_question_and_answer(q, response)
+        question_and_answer_audio = adjust_playback_speed(
+            question_and_answer_audio, speed=1.25
+        )
+
+        overall_audio += question_and_answer_audio
+
+        # Display each question, answer, and the corresponding audio
+        st.write("Question:", q)
+        st.write("Answer:", response)
+
+        with tempfile.NamedTemporaryFile(delete=True, suffix=".mp3") as fp:
+            question_and_answer_audio.export(fp.name, format="mp3")
+            st.audio(fp.name, format="audio/mp3")
+
+    # Save the combined audio
+    audio_path = os.path.join("audios", os.path.basename(pdf_path))
+    overall_audio.export(audio_path, format="mp3")
+    st.write("Combined Audio:")
+    st.audio(audio_path, format="audio/mp3")
+
+    return audio_path
+
+
+def adjust_playback_speed(audio, speed=1.0):
+    """
+    Adjust the playback speed of the audio.
+    :param audio: AudioSegment object
+    :param speed: desired speed (e.g., 1.25 for 1.25x speed)
+    :return: AudioSegment object with adjusted speed
+    """
+    return audio._spawn(
+        audio.raw_data, overrides={"frame_rate": int(audio.frame_rate * speed)}
+    ).set_frame_rate(audio.frame_rate)
 
 
 def main():
+    global history
+
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
-    st.header("Ask your PDF 💬")
-    
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
-    
-    # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
-        docs = knowledge_base.similarity_search(user_question)
-        
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
-        st.write(response)
-    
+    st.header("Ask your PDF ")
 
-if __name__ == '__main__':
+    display_history()
+
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    # Load pre-configured questions from the file
+    with open("questions.txt", "r") as f:
+        default_questions = f.readlines()
+
+    # Allow user to edit pre-configured questions
+    questions = st.text_area(
+        "Edit questions if needed:",
+        value="".join(default_questions),
+        height=200,
+    ).splitlines()
+
+    if st.button("Submit and Process") and pdf:
+        pdf_path = save_uploaded_pdf(pdf)
+        audio_path = process_questions_and_generate_audio(pdf_path, questions)
+
+        history.append(
+            {
+                "filename": os.path.basename(pdf_path),
+                "pdf_path": pdf_path,
+                "audio_path": audio_path,
+            }
+        )
+
+
+if __name__ == "__main__":
     main()
--- readme.md
+++ readme.md
@@ -2,36 +2,54 @@
 
 >You may find the step-by-step video tutorial to build this application [on Youtube](https://youtu.be/wUAUdEw5oxM).
 
-This is a Python application that allows you to load a PDF and ask questions about it using natural language. The application uses a LLM to generate a response about your PDF. The LLM will not answer questions unrelated to the document.
+This is a Python application that allows you to upload a PDF, ask questions about its content, and also pose pre-configured questions. The questions and their answers will be converted into audio format, which can be played directly within the app. The application leverages an LLM to generate the responses based on the content of the uploaded PDF.
+
+## Features
+- **Upload PDF**: Users can upload a PDF file to the application.
+- **Ask Custom Questions**: After uploading, users can type in custom questions about the content of the PDF.
+- **Pre-configured Questions**: The app also provides a set of pre-configured questions that users can edit. Once submitted, the app will automatically ask these questions about the PDF content.
+- **Audio Responses**: Questions and their corresponding answers are converted into audio. Users can play the audio directly within the app.
+- **History**: The app maintains a history of uploaded PDFs and their corresponding audio responses. Users can replay the audio or download the PDFs from the history.
 
 ## How it works
 
-The application reads the PDF and splits the text into smaller chunks that can be then fed into a LLM. It uses OpenAI embeddings to create vector representations of the chunks. The application then finds the chunks that are semantically similar to the question that the user asked and feeds those chunks to the LLM to generate a response.
+The application reads the uploaded PDF and extracts its text. This text is then split into manageable chunks suitable for processing by the LLM. The app utilizes OpenAI embeddings to create vector representations of these chunks. When a user poses a question, the application identifies text chunks that are semantically similar to the question. These relevant chunks are then provided to the LLM, which generates a response.
 
-The application uses Streamlit to create the GUI and Langchain to deal with the LLM.
+To facilitate user interaction, the application uses Streamlit for its GUI. The core logic and LLM interactions are handled by the Langchain library.
 
+## Prerequisites
+
+Before running the application, ensure you have `ffmpeg` and `ffprobe` installed as they are required for audio processing.
+
+- **macOS** (using Homebrew):
+  ```bash
+  brew install ffmpeg
+  ```
+- **Linux** (e.g., using apt for Ubuntu):
+  ```bash
+  sudo apt update
+  sudo apt install ffmpeg
+  ```
+- **Windows** 
+  Download the precompiled binaries from the [ffmpeg official website](https://ffmpeg.org/download.html). After downloading and extracting, add the path to the binaries to your system's PATH environment variable.
 
 ## Installation
+To set up the application:
 
-To install the repository, please clone this repository and install the requirements:
-
-```
-pip install -r requirements.txt
-```
-
-You will also need to add your OpenAI API key to the `.env` file.
+1. Clone this repository.
+1. Install the required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+1. Ensure you have the `uploads` and `audios` directories created in the root of the project to store the uploaded PDFs and generated audio files, respectively.
+1. Add your OpenAI API key to the .env file for the LLM interactions.
 
 ## Usage
-
-To use the application, run the `main.py` file with the streamlit CLI (after having installed streamlit): 
-
-```
+To use the application, run the `app.py` file with the streamlit CLI (after having installed streamlit):
+```bash
 streamlit run app.py
 ```
 
-
 ## Contributing
-
-This repository is for educational purposes only and is not intended to receive further contributions. It is supposed to be used as support material for the YouTube tutorial that shows how to build the project.
-
+This repository is primarily for educational purposes. While it is an enhancement of the original tutorial project, it is still intended to serve as reference material rather than an actively developed project. Contributions are not actively sought, but insights and suggestions are always welcome.
 
